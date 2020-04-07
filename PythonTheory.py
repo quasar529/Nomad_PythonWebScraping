4a1b808bb1e50aa800569025d148c09f28f78c97
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-04-07 15:16:00 +0900

    list

--- PythonTheory.py
+++ PythonTheory.py
@@ -1,9 +1,14 @@
 # 파이썬은 샵이 주석이다
 # 세미콜론 필요 없다
 '''작은따옴표 3개도 주석'''
-# type : int float bool str
+# type : int, float, bool, str, none
 # sequence type : list, tuple
 
-a=2
-b=3
-print(a+b)
+# list : list=[..]
+
+# 배열에 어떤 원소 있나 확인 가능 ex) print("Mon" in days)= True/False
+days=["Mon", "Tue", "Tue", "Thr", "Fri"]
+
+print("Mon" in days)
+days.append("Sat")
+print(days)
